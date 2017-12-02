beb4407f9227b7b8f12c86d20d92e52cb1ae3a63
JTu8 <32706871+JTu8@users.noreply.github.com>
2017-12-02 22:28:28 +0200

    Add files via upload

---
+++ checklogin.php
@@ -0,0 +1,83 @@
+﻿<?php
+	session_start();
+	
+	require_once("db.inc");
+	
+	function GetData($data) {
+		
+		$muuttuja = NULL;
+		if (isset($_POST[$data])) {
+			
+			$muuttuja = $_POST[$data];
+			
+		}
+		
+		return $muuttuja;
+	}
+	
+	if (isset($_POST["kirjaudu"])) {
+		
+		$kayttajatunnus = GetData("kayttajatunnus");
+		$salasana = GetData("salasana");
+		$tunnustyyppi = GetData("tunnustyyppi");
+		$vastaus_status = GetData("vastaus_status");
+		$kysely_id = GetData("kysely_id");
+		$kysymykset_id = GetData("kysymykset_id");
+		
+		
+		$kayttajatunnus = stripslashes($kayttajatunnus);
+		$salasana = stripslashes($salasana);
+		//$kayttajatunnus = mysqli_real_escape_string($kayttajatunnus);
+		//$salasana = mysqli_real_escape_string($salasana);
+		
+		$query = "SELECT * FROM tunnus WHERE tunnus = '$kayttajatunnus' AND salasana = '$salasana'";
+		$sql = "SELECT * FROM kysymykset WHERE kysely_id LIKE '$kysely_id'";
+		
+		
+		$tulos = mysqli_query($conn, $query);
+		$tulos2 = mysqli_query($conn, $sql);
+		
+		$count = mysqli_fetch_array($tulos);
+		
+		if ($count['tunnustyyppi'] == 'vastaaja' && $count["vastaus_status"] == 'ei vastannut') {
+			
+			if (!$tulos2) {
+				
+				echo "ID:n lähetys epäonnistui" .mysqli_error($conn);
+			}
+			else {
+				
+				$_SESSION['kysymykset_id'] = $_POST["kysymykset_id"];
+			}
+			$_SESSION['kayttajatunnus'] = $_POST["kayttajatunnus"];
+			
+			header("location:kyselylomake.html");
+		}
+		
+		elseif ($count['tunnustyyppi'] == 'admin') {
+			
+			$_SESSION['kayttajatunnus'] = $_POST["kayttajatunnus"];
+			header("location:admin_page.php");
+		}
+		
+		else {
+			
+			echo "Väärä käyttäjätunnus/salasana, tai olet jo vastannut kyselyyn";
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
+?>
+<html>
+	<head>
+		<title>Kalastusrekisteri</title>
+	</head>
+	<body>
+		<p><a href="login.php">Takaisin</a></p>
+	</body>
+</html>
---
+++ login.php
@@ -0,0 +1,47 @@
+﻿<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Kalastusrekisteri</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+		<link rel="stylesheet" type="text/css" href="projekti.css">
+		<script src="login.js"></script>
+	</head>
+	<body>
+		<div class="w3-container">
+			<h1><center>Tervetuloa</center></h1>
+			<h3><center>Klikkaa kirjaudu sisään-painiketta ja pääset sisään palveluun</center></h3>
+		</div>
+		
+		<div style="text-align:center;">
+			<button onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Kirjaudu sisään</button>
+		</div>
+		
+		<div id="id01" class="modal">
+			
+			<form class="modal-content animate" action="checklogin.php" method="post">
+				<div class="imgcontainer">
+					<span onclick="document.getElementById('id01').style.display='none'" class="close" title="Sulje modaali">&times;</span>
+					
+				</div>
+				<div class="container">
+					<label><b>Käyttäjänimi</b></label>
+					<input type="text" placeholder="Syötä käyttäjätunnus" name="kayttajatunnus" required>
+					
+					<label><b>Salasana</b></label>
+					<input type="password" placeholder="Syötä salasana" name="salasana" required>
+					
+					<button type="submit" name="kirjaudu">Kirjaudu sisään</button>
+					
+				</div>
+				<div class="container" style="background-color:#f1f1f1">
+					<button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Keskeytä</button>
+				</div>
+			</form>
+		</div>
+		
+		<div class="mySlides w3-display-container w3-center">
+			<img src="kuva1.jpeg" style="width:100%">
+		</div>
+	</body>
+</html>
